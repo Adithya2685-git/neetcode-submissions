daa4f4e1fd95033a786c53a3b191ad3158fea4b7
Adithya J <adithya.jillellamudi@students.iiit.ac.in>
2026-06-05 22:18:57 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+
+        let mut map : HashMap<char, i32> = HashMap::with_capacity(26);
+
+        for element in s.chars(){
+            
+            let count = map.entry(element).or_insert(0);
+            *count += 1;
+
+        }
+
+        for element in t.chars(){ 
+
+            let count = map.entry(element).or_insert(-1);
+            *count -=1;
+        }
+
+        for (key,val) in map.iter(){
+            
+            if *val != 0{
+                return false;
+            }
+        }
+        true
+        
+    }
+}
